e025ac23847db92d0267674d7d636042c9d84d0a
Fadilah Aulia <fadilahaulia469@gmail.com>
2026-04-25 18:08:20 +0700

    fix: update php to 8.4

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,13 +1,13 @@
-@extends('layout.template')  
-@section('title', 'Input Data Movie')  
+@extends('layout.template')
+@section('title', 'Input Data Movie')
 @section('content')
-		<a href="/movies/data" class="btn btn-primary mt-4">List Movie</a>
-		<h2 class="mb-4">Tambah Movie Baru</h2>
-        <form action="/movies/store" method="POST" enctype="multipart/form-data">
-			@csrf
-			@include('partials.movie-form-fields')
-			<div class="mb-3">
-				<button type="submit" class="btn btn-primary">Simpan</button>
-			</div>
-		</form>
-		@endsection
+    <a href="/movies/data" class="btn btn-primary mt-4">List Movie</a>
+    <h2 class="mb-4">Tambah Movie Baru</h2>
+    <form action="/movies" method="POST" enctype="multipart/form-data">
+        @csrf
+        @include('partials.movie-form-fields')
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </div>
+    </form>
+@endsection
