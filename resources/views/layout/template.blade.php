38d7881cbcf22705d510fd180186dd65515bca04
Fadilah Aulia <fadilahaulia469@gmail.com>
2026-04-25 17:51:53 +0700

    Perbaiki rute dan update composer

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -6,16 +6,15 @@
     <title>tiMovie - @yield('title', 'Website')</title>
     <link href="/bootstrap/bootstrap.min.css" rel="stylesheet">
   </head>
+
   <body>
-    
+      @include('partials.navbar')
+
       <div class="container my-2">
         @yield('content')
       </div>
 
-      <footer class="bg-success text-center text-white py-2">
-        Copyright &copy; 2023 by Yori Adi Atma
-      </footer>
-
+      @include('partials.navbar')
     <script src="/bootstrap/bootstrap.bundle.min.js"></script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MovieController::class, 'index'])->name('movies.index');
 Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movies.show');
 Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
-Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::post('/movies/store', [MovieController::class, 'store'])->name('movies.store');
 Route::get('/movies/data', [MovieController::class, 'data'])->name('movies.data');
 Route::get('/movies/{id}/edit', [MovieController::class, 'edit'])->name('movies.edit');
 Route::put('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
